8b8d30401ea2323ed0403762ba16e72647ea13d3
Binh2011 <binhit201195@gmail.com>
2017-08-19 15:04:42 +0700

    them Layout, Product, Detail, Layout Admin

---
+++ ShopOnline/Controllers/SanPhamController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ShopOnlineBus.Bus;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopOnline.Controllers
+{
+    public class SanPhamController : Controller
+    {
+            // GET: SanPham
+            public ActionResult Index()
+            {
+                return View(SanPhamBus.List());
+            }
+
+        // GET: SanPham/Details/5
+            public ActionResult Details(int id)
+            {
+                return View(SanPhamBus.GetById(id));
+            }
+    }
+}
---
+++ ShopOnlineBus/Bus/SanPhamBus.cs
@@ -0,0 +1,23 @@
+﻿using ShopOnlineConnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopOnlineBus.Bus
+{
+    public class SanPhamBus
+    {
+        public static IEnumerable<viewSanPham> List()
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.Query<viewSanPham>("SELECT * FROM viewSanPham");
+        }
+        public static SanPham GetById(int id)
+        {
+            var db = new ShopOnlineConnectionDB();
+            return db.SingleOrDefault<SanPham>("select * from SanPham where ID_SP = @0", id);
+        }
+    }
+}
